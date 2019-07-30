7b48b68e3c9a5437f8af51cc51ab43208b6e830c
Romich <roman.lukashenko@outlook.com>
2019-07-30 18:37:13 +0300

    async

--- ticker.py
+++ ticker.py
@@ -5,42 +5,50 @@ from time import sleep
 from datetime import datetime
 from os import getcwd
 
+CURRENT_TIME = ""
 
-def gather_ticker():
 
-    #async def poloniex():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+async def poloniex():
 
-    async with requests.get('https://poloniex.com/public?command=returnTicker') as response:
+    with requests.get('https://poloniex.com/public?command=returnTicker') as response:
         text = await response.text
         with open(getcwd() + '/ticker/poloniex.csv', 'a+') as f:
-            f.write(current_time+','+text+'\n')
-        print(current_time + 'Poloniex')
+            f.write(CURRENT_TIME+','+text+'\n')
+        print(CURRENT_TIME + 'Poloniex')
 
-    #async def binance():
-    async with requests.get('https://api.binance.com/api/v3/ticker/bookTicker') as response:
+
+async def binance():
+
+    with requests.get('https://api.binance.com/api/v3/ticker/bookTicker') as response:
         text = await response.text
         with open(getcwd() + '/ticker/bitfinex.csv', 'a+') as f:
-            f.write(current_time + ',' + text + '\n')
-        print(current_time + 'binance')
+            f.write(CURRENT_TIME + ',' + text + '\n')
+        print(CURRENT_TIME + 'binance')
 
-    #async def bitfinex():
-    async with requests.get('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL') as response:
+
+async def bitfinex():
+    with requests.get('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL') as response:
         text = await response.text
         with open(getcwd() + '/ticker/bitfinex.csv', 'a+') as f:
-            f.write(current_time + ',' + text + '\n')
-        print(current_time + 'binance')
+            f.write(CURRENT_TIME + ',' + text + '\n')
+        print(CURRENT_TIME + 'binance')
 
 
 def main():
 
     counter = 0
     while counter < 10:
-        gather_ticker()
+        CURRENT_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(lambda x, y: None)  # suppress exceptions because of bug in Python 3.7.3 + aiohttp + asyncio
+        loop.run_until_complete(asyncio.ensure_future(asyncio.gather(poloniex(), binance(), bitfinex())))
+
         sleep(1)
         counter += 1
-        if counter % 10 == 0:
+        if counter % 1 == 0:
             print(str(counter))
+
 
 if __name__ == '__main__':
     main()
